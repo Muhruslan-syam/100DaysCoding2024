8ddac2af26a54559e5c8fb8fedd007f711436d04
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-05 12:12:12 +0800

    Hari 4
    Menginput bidata nama nim dan tinggi badan menggunakan perintah printf

---
+++ Day4.java
@@ -0,0 +1,7 @@
+public class day4 {
+    
+    public static void main(String[] args){
+        System.out.printf("NAMA = %s,NIM = %s,tinggi badan= %f","muh ruslan syam","D0224037",58.5);
+    }
+    
+    }
