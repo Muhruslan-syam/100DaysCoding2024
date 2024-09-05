7ff54f5c2bad3dd0d38cc6bc7c67faacbec41637
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-06 07:32:28 +0800

    Hari 5
    Membuat komentar

---
+++ Day5.java
@@ -0,0 +1,9 @@
+public class day5 {
+    
+    public static void main(String[] args) {
+        //System.out.println(3+2);
+        //System.out.println(5);
+        System.out.println(2);
+    }
+    
+}
