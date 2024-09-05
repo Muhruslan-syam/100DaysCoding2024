694d426b820b844b386f6437180344486c5d4f0e
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-05 12:26:34 +0800

    Hari 4
    Menginput biodata nama, NIM, umur dantinggi badan menggunakan perintah printf

--- Day4.java
+++ Day4.java
@@ -1,7 +1,7 @@
 public class day4 {
     
     public static void main(String[] args){
-        System.out.printf("NAMA = %s,NIM = %s,umur = %d,tinggi badan= %f","muh ruslan syam","D0224037","18"158.5);
+        System.out.printf("NAMA = %s,NIM = %s,umur = %d,tinggi badan= %f","muh ruslan syam","D0224037",18,158.5);
     }
     
     }
