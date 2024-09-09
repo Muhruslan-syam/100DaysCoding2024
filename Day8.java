14ee7bcbd32c42e26bf53a5355699a93b4f808a1
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-09 23:43:50 +0800

    Harinke 8
    Variabel update

---
+++ Day8.java
@@ -0,0 +1,14 @@
+public class day8 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // manginisialisasikan variabel
+        int nama = 2;
+ 
+        //mendeklarasikan variabel
+        System.out.println("nama ="+ nama);
+        
+        
+    }
+    
+}
