18216ea259dec2095e0bf3a810f2ae3be4cc40fb
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-10 23:05:12 +0800

    Harike 9
    Variabel kontan yaitu nilai tetap

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class day9 {
+    
+    public static void main(String[] args) {
+        // manginisialisasikan variabel
+        final int nama = 2;
+        final String Nama = "huruf";
+        final double tinggi_badan = 160.0;
+        
+        
+    }
+    
+}
