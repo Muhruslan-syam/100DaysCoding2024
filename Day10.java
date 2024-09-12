6f002fb6a751ee1ba97ad5637aa2936ac9615b8e
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-12 20:15:59 +0800

    Harike 10

---
+++ Day10.java
@@ -0,0 +1,10 @@
+public class day10{
+    
+    public static void main(String[] args) {
+        // manginisialisasikan variabel
+        short Hutang1 = 150;
+        System.out.print(Hutang1);
+         
+    }
+    
+}
