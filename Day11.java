b1a0c093599331b7fbffa26cc6854360ad079c78
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-13 20:08:53 +0800

    Hari ke 11
    Membuat variabel int

---
+++ Day11.java
@@ -0,0 +1,10 @@
+public class day11{
+    
+    public static void main(String[] args) {
+        // manginisialisasikan variabel
+        int Hutang1 = 150;
+        System.out.print(Hutang1);
+         
+    }
+    
+}
