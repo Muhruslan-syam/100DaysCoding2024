3bdc1ff8761c8616408455558e717b96be55d3f0
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-14 10:42:10 +0800

    Hari ke 12
    Membuat variabel update

---
+++ Day12.java
@@ -0,0 +1,12 @@
+public class day12{
+    
+    public static void main(String[] args) {
+        // manginisialisasikan variabel
+        char B = 1;
+        System.out.print(Hutang1);
+        B = 2;
+        System.out.println(B);
+         
+    }
+    
+}
