eabbc49e38e41ad648b30c50659c4b02d2913639
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-26 22:51:34 +0800

    Hari ke 25
    Membuat fariabel referensi double

--- Day25.java
+++ Day25.java
@@ -2,7 +2,7 @@ public class day25{
     
     public static void main(String[] args) {
         Double A = 123224.5;
-        System.out.println("A")
+        System.out.println(A)
         
 
     }
