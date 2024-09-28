51022700f40d87692750ac0baf029955ea8f6d97
Muhruslan-syam <rsyam8225@gmail.com>
2024-09-28 22:14:50 +0800

    Hari ke 27
    Tugas evaluasi

---
+++ Day27.java
@@ -0,0 +1,18 @@
+public class belajar {
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("selamat datang ");
+        int jumlahTanaman = 20;//konstanta
+        String namaTanaman = "melati";
+        double TinggiTanaman = 25.3;
+        namaTanaman = "mawar";
+        System.out.printf("nama bunga nya %s tinggi bunga tersebut %.2f jumlah tanaman yang tersisa %.2f ",namaTanaman,TinggiTanaman,jumlahTanaman);
+            
+        
+        
+        
+    }
+    
+}
