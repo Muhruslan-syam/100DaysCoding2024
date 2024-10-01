0fb6d1df89aac1aa2c9c06bd297f153fc1bf4354
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-01 09:34:30 +0800

    Hari ke 29
    Membuat tipe data referensi BigInteger

---
+++ Day29.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+public class reference {
+    
+    public static void main(String[] args) {
+        BigInteger A = new BigInteger("12345678901234567890");
+        System.out.print("BigInteger: " + A);
+    }
+    
+}
