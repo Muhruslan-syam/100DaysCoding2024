c5e37ee025672afc2e432718b859622b37c2f7f0
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-01 19:27:34 +0800

    Hari ke 30
    Membuat tipedata referensi bigdecimal

---
+++ Day30.java
@@ -0,0 +1,8 @@
+nimport java.math.BigDecimal;
+public class belajar{
+    
+    public static void main(String[] args) {
+        
+        BigDecimal a =new BigDecimal("1230000000000000000000000000000000");
+        
+        System.out.println(a);
