09d76b6511e6e5d98a5de9a06ec16d9539c121a4
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-03 06:54:35 +0800

    Hari ke 31

---
+++ Day31.java
@@ -0,0 +1,17 @@
+public class penegasan {
+    public static void main(String[]args){
+        String x ="533";
+        int y=
+        Integer.parseInt(x);
+        System.out.println(y+4);
+        String p ="1435.6";
+        double q =
+        Double.parseDouble(p);
+        System.out.println(q+2.6);
+        String a ="true";
+        boolean b=
+        Boolean.parseBoolean(a);
+        System.out.println(!b);
+    }
+    
+}
