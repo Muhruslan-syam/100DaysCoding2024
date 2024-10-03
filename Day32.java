570f6222723d5e5e23c554ab425f5558cf9fbfc6
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-03 18:27:17 +0800

    Hari ke 32
    Mengkonversi tipe data primitif menjadi string

---
+++ Day32.java
@@ -0,0 +1,18 @@
+
+public class conversi {
+    public static void main(String[]args){
+        double a = 142;
+        String x = String.valueOf(no);
+        System.out.print(x);
+        int b = 234;
+        String c = String.valueOf(b);
+        System.out.print(c);
+        float d = 234f;
+        String e = String.valueOf(d);
+        System.out.print(e);
+        char f = '&';
+        String g = String.valueOf(f);
+        System.out.print(g);
+    }
+    
+}
