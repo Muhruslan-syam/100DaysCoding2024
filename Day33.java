f9cc741865470145ad2ae05258bef27fa79b600d
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-04 22:22:37 +0800

    Hari ke 33
    Membuat tipe casting atau pertukaran tipe data

---
+++ Day33.java
@@ -0,0 +1,18 @@
+public class belajar{
+    
+    public static void main(String[]args){
+        double a = 200;
+        int b = (int)a;
+        System.out.println(b);
+        float c = b;
+        System.out.println(c);
+        byte d = (byte)c;
+        System.out.println(d);
+        
+        
+        
+        
+        
+    }
+    
+}
