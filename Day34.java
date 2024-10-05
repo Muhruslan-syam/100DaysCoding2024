2da3f2adc2308b28dea998282c3d2cf0e95ee449
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-05 20:51:02 +0800

    Hari ke 34
    Membuat program input keyboard menggunakan scanner

---
+++ Day34.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class penegasan{
+    
+    public static void main(String[]args){
+        Scanner a = new Scanner(System.in);
+        System.out.print("input nama =");
+        String b = a.nextLine();
+        System.out.print("input kelas =");
+        String c   = a.nextLine();
+        System.out.print("input NIM =");
+        String d  = a.nextLine();
+        System.out.print("HALOO");
+        System.out.printf("Nama saya %s kelas %s Nim %s",b,c,d);
+        
+        
+        
+        
+        
+    }
+    
+}
