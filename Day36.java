be8883634d9fb6855f45c0d62dca6b7fd587cd58
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-07 05:55:54 +0800

    Hari ke 36
    Membuat aritmatika perkalian pengurangan dan mkdulo

---
+++ Day36.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class aritmatika{
+    
+    public static void main(String[]args){
+        Scanner a = new Scanner(System.in);
+        System.out.print("input nilai =");
+        int b = a.nextInt();
+        System.out.print("input nilai =");
+        int c   = a.nextInt();
+ 
+        System.out.println("hasil = "+ b*c);
+        System.out.println("hasil = "+ b/c);
+        System.out.println("hasil = "+ b%c);
+        
+        
+        
+        
+        
+    }
+    
+}
