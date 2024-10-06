12dd0e2d116ae620c475d6280de2f2f8768aa062
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-07 05:41:23 +0800

    Hari ke 35
    Membuat aritmatika

---
+++ Day35.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class penegasan{
+    
+    public static void main(String[]args){
+        Scanner a = new Scanner(System.in);
+        System.out.print("input nilai =");
+        int b = a.nextInt();
+        System.out.print("input nilai =");
+        int c   = a.nextInt();
+        int d  = b*c;
+        System.out.printf("hasil = ",d);
+        
+        
+        
+        
+        
+    }
+    
+}
