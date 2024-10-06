6ecbbf4e7a2d4efa883cc4bdda722d0b404d969b
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-07 06:02:55 +0800

    Hari ke 37
    Penugasan

---
+++ Day37.java
@@ -0,0 +1,17 @@
+public class penugasan{
+    
+    public static void main(String[]args){
+        int a = 4;
+        a +=2;
+        System.out.println("hasil penumlahan = "+ a);
+        a -= 1;
+        System.out.println("hasil pengurangan = "+ a);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
