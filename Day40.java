5fa03f8df6169d3c5b51aa05399854d89c30110f
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-10 21:09:32 +0800

    Hari ke 40
    Membuat operator perbandingan

---
+++ Day40.java
@@ -0,0 +1,14 @@
+package pelatihan;
+
+public class Main {
+    
+    public static void main(String [] args) {
+        int a;
+        a = 9;
+         System.out.print(a>=3);
+        
+        
+        
+    }
+    
+}
