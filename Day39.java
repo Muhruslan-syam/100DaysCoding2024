a93971d7d83e76589b0af8fee05442f783c05509
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-10 18:57:51 +0800

    Hari ke 39
    Membuat operator penugasan pengisian dan modulo

---
+++ Day39.java
@@ -0,0 +1,14 @@
+package pelatihan;
+
+public class Main {
+    
+    public static void main(String [] args) {
+        int a;
+        a = 9;
+        a %= 3;
+        
+        
+        
+    }
+    
+}
