68a69b46b5e822d5005738b14ff54de6e287f51c
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-14 09:21:58 +0800

    Hari ke 42
    Operator perbandingan sama dengan dan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,16 @@
+package day42;
+
+public class Main {
+    
+    public static void main(String [] args) {
+        int a;
+        a = 9;
+         System.out.print(a == 9);
+        System.out.print(3 != a);
+        
+        
+        
+        
+    }
+    
+} 
