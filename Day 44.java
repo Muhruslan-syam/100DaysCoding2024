01d751853c6774a21965c8d40abbf97e13441a1f
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-15 17:06:57 +0800

    Hari ke 44
    Operator perbandingan dan dan

---
+++ Day 44.java
@@ -0,0 +1,16 @@
+package day44;
+
+public class Main {
+    
+    public static void main(String [] args) {
+        int a;
+        a = 9;
+         System.out.print(a >= 9 && 3 <= a);
+     
+        
+        
+        
+        
+    }
+    
+} 
