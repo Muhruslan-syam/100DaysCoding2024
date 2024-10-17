c9d52b10f19f20df0cb96ee584f36e8793200ac4
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-17 22:29:40 +0800

    Hari 46
    Not

---
+++ Day46.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class b {
+    public static void main(String[] args) {
+        Scanner a = new Scanner (System.in);
+        System.out.print("masukkan angka");
+        
+
+            int b = a.nextInt();
+            boolean c = b >= 3;
+        
+            System.out.print(!c);
+        
+        
+
+        
+        
+    }
+}
