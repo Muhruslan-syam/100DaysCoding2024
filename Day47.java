f5b889471dcaefa4f5711604dfa645ffd5ace9f2
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-18 19:55:22 +0800

    Hari ke 47
    Percabangan

---
+++ Day47.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class b {
+    public static void main(String[] args) {
+        Scanner a = new Scanner (System.in);
+        System.out.print("masukkan angka");
+        
+            int b = a.nextInt();
+            
+        if (b > 0){
+            System.out.println("ini angka = ");
+        }
+        
+    }
+}
