fb5229a2cac6fff6c9b859207f7aa7ce8c66f355
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-19 20:50:51 +0800

    Hari ke 48
    Percabangan if else

---
+++ Day48.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class b {
+    public static void main(String[] args) {
+        Scanner a = new Scanner (System.in);
+        System.out.print("masukkan angka = ");
+        
+        while(true){
+            int b = a.nextInt();
+        
+        if (b > 0){
+            System.out.println("ini angka posistif = "+b);
+            System.out.print("masukkan angka = ");
+        }
+        else if (b<0){
+            System.out.println("ini angka negatif = "+b);
+            System.out.print("masukkan angka = ");
+        }
+            
+            else {
+            System.out.println("ini angka nol = "+b);
+                System.out.print("masukkan angka = ");
+        }
+            
+        }
+    }
+}
