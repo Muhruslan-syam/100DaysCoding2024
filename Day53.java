a394071ec44d2ca1098be1ca5d85e9af0ddab60c
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-24 20:11:08 +0800

    Hari ke 53
    Membuat perulangan for

---
+++ Day53.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class day53 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.println("masukkan angka: ");
+		int a = input.nextInt();
+		for (int i = 1; i <10; i++){
+			 System.out.print(i);
+		}
+	}
+}
