949014447d8922170cfb1451f314593a58cbb4c7
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-25 23:17:31 +0800

    Hari ke 54
    For increment

---
+++ Day54.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ahkuis {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.println("masukkan angka: ");
+		int a = input.nextInt();
+		for (int i = 1; i <10; i++){
+			 System.out.print(""+i);
+		}
+	}
+}
