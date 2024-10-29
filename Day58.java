b91e87bca6b4b2b0e9b07255c7827624cee21401
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-29 22:25:42 +0800

    Hari ke 58
    While dekremen

---
+++ Day58.java
@@ -0,0 +1,9 @@
+public class ahkuis {
+    public static void main (String[]args){
+        int a = 40;
+        while (a>2){
+            System.out.println("nilai a adalah :"+a);
+            a-=3;
+        }
+    }
+}
