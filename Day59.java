d7066dfc0cd42d0b9579ca0e225b16e0a6e09202
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-30 22:47:45 +0800

    Harike 59

---
+++ Day59.java
@@ -0,0 +1,8 @@
+public class hhhh {
+    public static void main (String[]args){
+        int[]a={1,2,3,4,5,6,7,8};
+        for(int angka:a){
+            System.out.print(angka+"  ");
+        }
+    }
+}
