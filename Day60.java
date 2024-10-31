6b2748cb10c35f653cb5ebf69be32ab0572efb51
Muhruslan-syam <rsyam8225@gmail.com>
2024-10-31 21:40:07 +0800

    Harike 60
    Membuat perulangan di while

---
+++ Day60.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        
+        int n =1;
+        
+        do {
+            System.out.print(n);
+            n++;
+        }
+        while(n<=10);
+    }
+}
