c7e15ac43b9970ac0dad2f2cc9e3ad5794175c4a
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-03 22:50:51 +0800

    Hari ke 62
    Break

---
+++ Day62.java
@@ -0,0 +1,11 @@
+public class ahkuis {
+    public static void main(String[] args) {
+        
+        int n =5; // ukuran persegi
+        
+        for(int i = 0; i <= n ; i++){
+            System.out.print(i);
+            break;
+        }
+    }
+}
