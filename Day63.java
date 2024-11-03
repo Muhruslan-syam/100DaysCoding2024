30b206491f84c67d35cc9fbc29765f9bb7f66dcb
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-03 22:43:35 +0800

    Hari ke 63

---
+++ Day63.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main (String[]args){
+        for (int b = 5; b<=11; b++){
+           if (i==5){
+                    continue; 
+           }
+           System.out.println(b);
+      
+        }
+    }
+}
