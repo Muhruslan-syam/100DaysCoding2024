a69f71f1552286fa5ec59ced3de70c82c50a8e9c
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-04 19:56:40 +0800

    Hari ke 64
    Membuat pelabelan break

---
+++ Day64.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class b {
+    public static void main(String[] args) {
+        outerLoop: //ink untuk pelabelan break
+            for(int b = 0; b<10 ; b++ ){
+            System.out.println("jumlah perulangan : "+b);
+            if (b == 7) {
+                    System.out.println("Telah mencapai batas!!");
+                break outerLoop;
+                }
+            }
+        
+        }
+            
+        
+    }
