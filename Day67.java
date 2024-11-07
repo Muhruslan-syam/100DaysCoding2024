f7559ba5eb81ac1b6bc8064ce9d1ebd974baf9a1
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-07 22:00:57 +0800

    Hari ke 67
    Membuat method void

---
+++ Day67.java
@@ -0,0 +1,14 @@
+public class ahkuis{
+
+    // Method void untuk mencetak pesan
+    public static void A() {
+        System.out.println("Halo, Selamat Datang di Program Java!");
+    }
+
+    public static void main(String[] args) {
+        // untuk memanggil method A dan menampilkan isi dari A
+        A();
+        
+        
+    }
+}
