c597a3f0bb91511c65878d81f27d6af38afb1244
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-10 00:15:49 +0800

    Harike 68

---
+++ Day68.java
@@ -0,0 +1,13 @@
+public class b {
+
+    
+    public int Cullang(int a, int b) {
+        return a % b;
+    }
+
+    public static void main(String[] args) {
+        Fariabel c = new Fariabel();
+        double hasil = c.Cullang(2, 3);
+        System.out.println("Hasil: " + hasil);
+    }
+}
