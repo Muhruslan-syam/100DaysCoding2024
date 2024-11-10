406533ac8ff6d14b4c4f93f773ae9ddf36de59d3
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-11 00:57:03 +0800

    Harike 70
    Method parameter

---
+++ Day70.java
@@ -0,0 +1,9 @@
+public class b{
+         public void ket(String nama, String nim){
+                      System.out.println("nama "+nama+", nim"+nim);
+             }
+         public static void main (String[]args){
+             b b= new b();
+                  b.ket("cullang","D0224037");
+    }
+}
