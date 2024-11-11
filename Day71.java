f4ef84fccc2b71f2c3f1870cad5e580e46b9d237
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-11 23:38:56 +0800

    Hari ke 71
    Method static

---
+++ Day71.java
@@ -0,0 +1,9 @@
+public class b {
+             public static void a(){
+                    System.out.print("Jangan berhenti menjadi orang baik ");      
+             }
+
+         public static void main (String[]args){
+             serah.a();
+    }
+}
