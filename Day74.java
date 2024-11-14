cab374816eb2d29617070d3751eb895488904d06
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-14 20:34:39 +0800

    Hari ke 74
    Membuat method lenght

---
+++ Day74.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class charat {
+    
+    public static void main(String[] args) {
+        Scanner a = new Scanner (System.in);
+        System.out.println("masukkan kata = ");
+        String b = a.nextLine();
+        System.out.print("huruf dari kata yang anda masukan berjumlah = "+b.length());
+            
+        
+    }
+    
+}
