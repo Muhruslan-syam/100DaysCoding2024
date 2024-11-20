d35535b1f49df75ab646a95f7b0fdb46e754c782
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-21 07:34:50 +0800

    hari ke 80
    metode. trim()  yang digunakan untuk menghapus spasi dari awal teks dan akhir teks

--- day80.java
+++ day80.java
@@ -3,7 +3,7 @@ public class b{
     public static void main(String[]args){
         
         String s = "       iyaaaaaaaa      ";
-        String trimmedText = s.trim();
+        String b = s.trim();
         System.out.println("Kata asli: '" + s + "'");
         System.out.println("Kata yang sudah di ibuh: '" + trimmedText + "'");
     }
