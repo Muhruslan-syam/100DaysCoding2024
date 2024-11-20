6585bf7af9a1f55aa06424e4673b759f31a5db53
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-20 22:04:58 +0800

    herike 80
    membuat method trim()
    menghilangkan spasi di awal kalimat dan akhir kalimat

---
+++ day80.java
@@ -0,0 +1,10 @@
+public class b{
+
+    public static void main(String[]args){
+        
+        String s = "       iyaaaaaaaa      ";
+        String trimmedText = s.trim();
+        System.out.println("Kata asli: '" + s + "'");
+        System.out.println("Kata yang sudah di ibuh: '" + trimmedText + "'");
+    }
+}
