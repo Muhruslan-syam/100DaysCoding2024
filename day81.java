573718fe182856455d818a08801f981482f76d53
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-21 21:50:32 +0800

    hari ke 81
    method velueof ()
    mengkonversi tipe data menjadi tipe data lain

---
+++ day81.java
@@ -0,0 +1,21 @@
+public class penyelesaian {
+    
+    public static void main(String[] args) {
+        
+          
+          String a = "true";
+          Boolean b = Boolean.valueOf(a);
+           System.out.println(b);
+          
+          int c = 10;
+          String d = String.valueOf(c);
+          int e = 3;
+           System.out.println(d+e);
+          
+          
+          
+        
+            
+    }
+    
+}
