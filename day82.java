378bf21544db3ab2c23eb738fa4deb8b05b9d2fe
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-22 22:08:45 +0800

    harike 82
    membuat method ceil(), flor(), dan round()

---
+++ day82.java
@@ -0,0 +1,24 @@
+public class penyelesaian {
+    
+    public static void main(String[] args) {
+        
+          double a = 3.2;
+          double b = Math.ceil(a);
+          System.out.println("ini hasil seteleah dibulatkan = "+b);//////
+          
+          double c = 3.7;
+          double d = Math.floor(c);
+          System.out.println("ini hasil seteleah dibulatkan = "+d);
+          
+          double e = 3.5;
+          double f = 3.4;
+          double g = Math.round(e);
+          double h = Math.round(f);
+          System.out.println("ini hasil setelah dibulatkan = "+g);
+          System.out.println("ini hasil setelah dibulatkan = "+h);
+          
+        
+            
+    }
+    
+}
