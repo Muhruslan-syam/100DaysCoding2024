d94478f2cabb3a9ed9519b722e487312c89f5e85
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-23 23:58:10 +0800

    hari 83
    membuat array 1 dimensi

---
+++ day83.java
@@ -0,0 +1,14 @@
+public class array {
+    
+    public static void main(String[] args) {
+        String []satu = {"tigaroda","motor"};
+        
+        System.out.printf("[ %s , %s ]",satu[0],satu[1]);
+        
+        
+        
+        
+        
+    }
+    
+}
