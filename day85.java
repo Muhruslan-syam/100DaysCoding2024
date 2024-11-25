cffdea79a87470c8c9a88c61015efef09290f2a5
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-25 23:20:26 +0800

    hari ke 85
    membuat elemen array

---
+++ day85.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class array {
+    public static void main(String[]Args){
+        int [] a = {10,2,3};
+        System.out.print("ini elemen array = "+a[0]);
+    }
+}
+ 
