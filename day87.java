374f7d5dee41f549f9e983594a8c7d94e8311af0
Muhruslan-syam <rsyam8225@gmail.com>
2024-11-27 18:39:08 +0800

    harike 87
    mengubah isi array 1 dimensi dalam index tertentu

---
+++ day87.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class array {
+    public static void main(String[]Args){
+        int [] a = {10,2,3};
+        a[1]+=1;
+        System.out.print("ini elemen array = "+a[0]+","+a[1]+","+a[2]);
+    }
+}
+ 
