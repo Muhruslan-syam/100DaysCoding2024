f0242ca68c1c0f8ed3f5891e9cdf34344e9e8532
Muhruslan-syam <rsyam8225@gmail.com>
2024-12-02 07:00:52 +0800

    Harike 91
    You and i

---
+++ Day91.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class latihan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan kata pertama: ");
+        String a = sc.nextLine();
+        
+        System.out.print("Masukkan kata kedua: ");
+        String b = scannerl.nextLine();
+        
+        
+        if (a.equals("you") && b .equals("i")) {
+            System.out.println("you and i");
+        } else {
+            System.out.println("end");
+        }
+        
+    }
+}
